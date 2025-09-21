46d512083e3b2a3c84a6d9d3b420ba06bca0871d
Amal1906 <101694618+Amal1906@users.noreply.github.com>
2025-09-21 12:33:08 +0530

    Delete app directory

--- app/data/pipe/format-date-time.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FormatDateTimePipe } from './format-date-time.pipe';
-
-describe('FormatDateTimePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FormatDateTimePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
